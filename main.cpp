527c4c271cee8fecd43f5389156751bb01f80f1f
AlexBowsunowski <63734939+AlexBowsunowski@users.noreply.github.com>
2021-03-15 12:08:02 +0300

    upd

--- main.cpp
+++ main.cpp
@@ -13,66 +13,65 @@ int main() {
     std::cout<<"Your random list:\n";
     print_list(head);
     std::cout<<"Command list:\n";
-    std::cout<<"--Push Elem\n";
-    std::cout<<"--PushFront Elem ElemFront\n";
-    std::cout<<"--PushBack Elem ElemBack\n";
-    std::cout<<"--PushEnd Elem\n";
-    std::cout<<"--PushRepeat Elem ElemBack\n";
-    std::cout<<"--PopFront\n";
-    std::cout<<"--PopBack\n";
-    std::cout<<"--Pop Elem\n";
-    std::cout<<"--PrintList\n";
-    std::cout<<"--Cancel\n";
+    std::cout<<"1 Push Elem)\n";
+    std::cout<<"2 PushFront Elem ElemFront\n";
+    std::cout<<"3 PushBack Elem ElemBack\n";
+    std::cout<<"4 PushEnd Elem\n";
+    std::cout<<"5 PushRepeat Elem ElemBack\n";
+    std::cout<<"6 PopFront\n";
+    std::cout<<"7 PopBack\n";
+    std::cout<<"8 Pop Elem\n";
+    std::cout<<"9 PrintList\n";
+    std::cout<<"10 Cancel\n";
+    /*For example,
+     * Please input command:
+     * 1(Push)
+     * 3(Elem)
+     * 2(PushFront)
+     * 2(Elem) 3(ElemFront)
+     * */
     std::cout<<"Please input command:\n";
-    char* command = (char*) malloc(sizeof(char) * MAXSIZE);
-    fgets(command,MAXSIZE, stdin);
-    while(strlen(command) != 0){
-        char *istr = strtok(command, " ");
-        if(strcmp(istr, "Push") == 0){
-            istr = strtok(NULL, " ");
-            int el = atoi(istr);
+    int command;
+    std::cin>>command;
+    while(command != 10){
+        if(command == 1){
+            int el;
+            std::cin>>el;
             push(head,el);
-        }else if(strcmp(istr, "PushFront") == 0){
-            istr = strtok(NULL, " ");
-            int el = atoi(istr);
-            istr = strtok(NULL, " ");
-            int front = atoi(istr);
-            push_front(head,el, front);
-        }else if(strcmp(istr, "PushBack") == 0){
-            istr = strtok(NULL, " ");
-            int el = atoi(istr);
-            istr = strtok(NULL, " ");
-            int back = atoi(istr);
-            push_back(head,el, back);
-        }else if(strcmp(istr, "PushEnd") == 0){
-            istr = strtok(NULL, " ");
-            int el = atoi(istr);
+        }else if(command == 2){
+            int el, el_f;
+            std::cin>>el;
+            std::cin>>el_f;
+            push_front(head,el, el_f);
+        }else if(command == 3){
+            int el, el_b;
+            std::cin>>el;
+            std::cin>>el_b;
+            push_back(head,el, el_b);
+        }else if(command == 4){
+            int el;
+            std::cin>>el;
             push_end(head,el);
-        }else if(strcmp(istr, "PushRepeat") == 0){
-            istr = strtok(NULL, " ");
-            int el = atoi(istr);
-            istr = strtok(NULL, " ");
-            int back = atoi(istr);
-            push_repeat(head,el, back);
-        }else if(strcmp(istr, "PopFront") == 10){
+        }else if(command == 5){
+            int el, el_b;
+            std::cin>>el;
+            std::cin>>el_b;
+            push_repeat(head,el, el_b);
+        }else if(command == 6){
             int el = pop_front(head);
             std::cout<<el<<"\n";
-            istr = strtok(NULL, " ");
-        }else if(strcmp(istr, "PopBack") == 10){
+        }else if(command == 7){
             int el = pop_back(head);
             std::cout<<el<<"\n";
-            istr = strtok(NULL, " ");
-        }else if(strcmp(istr, "Pop") == 0){
-            istr = strtok(NULL, " ");
-            int elem = atoi(istr);
+        }else if(command == 8){
+            int elem;
+            std::cin>>elem;
             int el = pop_element(head, elem);
             std::cout<<el<<"\n";
-        }else if(strcmp(istr, "PrintList") == 10){
+        }else if(command == 9){
             print_list(head);
-        }else if(strcmp(istr, "Cancel") == 10){
-            break;
         }
-        fgets(command, MAXSIZE, stdin);
+        std::cin>>command;
     }
     return 0;
 }
